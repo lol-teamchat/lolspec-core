0111c9a852eabbd15e49fec7c6e27cbc1826124f
Adam Schachne <adamschachne@live.com>
2017-02-27 12:20:36 -0800

    began working on audio sync plugin. changed some overwolf app files

---
+++ audiosync/audiosync/audiosync.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace audiosync
+{
+    public class audiosync
+    {
+        // constructor
+        public audiosync() {
+            
+            
+        }
+
+        public void getTime(Action<object> callback) {
+            if (callback == null) {
+                return;
+            }
+
+            // get the current time of the game
+
+            return;
+        }
+
+        static int Main(string[] args) {
+            var injector = new LeagueInjector();
+            var dll = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\{"LeagueReplayHook.dll"}";
+            Console.WriteLine(dll);
+            if (injector.Inject(dll) == true) {
+                Console.WriteLine(dll + " injected successfully");
+            }
+            else {
+                Console.WriteLine(dll + " failed to inject.");
+                return 0;
+            }
+
+            int udp_port = 7000;
+            var listener = new Listener(udp_port);
+
+            while (true) {
+                Console.WriteLine(listener.GetMessage());
+            }
+            //Console.ReadKey();
+            //return 0;
+        }
+    }    
+}
--- overwolf-app/script.js
+++ overwolf-app/script.js
@@ -23,14 +23,24 @@ function closeWindow(){
 	});
 };
 
-function takeScreenshot(){
-	overwolf.media.takeScreenshot(function(result){
-		if (result.status == "success"){
-			var img = document.getElementById("screenshot");
-			img.src = result.url;
-			img.onload = function() {
-				overwolf.media.shareImage(img, "Screen Shot");
-			};
-		}
+var plugin = new OverwolfPlugin("testplugin", true);
+
+plugin.initialize(function(status) {
+	if (status == false) {
+		document.querySelector('#title').innerText = "Plugin couldn't be loaded??";
+		return;
+	}
+
+	plugin.get().add(5, 4, function(result) {
+    	console.log("5 + 4 = " + result);
+  	});
+
+	plugin.get().add(1, 2, function(result) {
+    	console.log("1 + 2 = " + result);
+  	});
+
+	plugin.get().getShpit(function(result) {
+		console.log(result);
 	});
-}
+
+});
